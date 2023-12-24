4587161c4375f33995aa412137d826163780f9b1
Agrim <agrimsharma1714@gmail.com>
2023-12-24 16:52:20 +0530

    project added

--- data/Projects.js
+++ data/Projects.js
@@ -1,16 +1,6 @@
 export const Projects = [
   {
     id: 1,
-    name: "Agrim Wear",
-    slug: "agrimwear",
-    img: "agrimwear",
-    demo: "https://agrimwear.vercel.app/",
-    sourceCode: "https://github.com/Agrim-Sharma174/agrimwear",
-    description:
-      "An e-commerce website built using NextJs, TailwindCSS. It has features of cart, wishlist, checkout, payment,  etc. It is deployed on Vercel.",
-  },
-  {
-    id: 2,
     name: "GrimChat",
     slug: "grimchat",
     img: "grim-chat",
@@ -21,6 +11,26 @@ export const Projects = [
   },
   {
     id: 2,
+    name: "Lanzzer",
+    slug: "lanzzer",
+    img: "lanzzerDashboard",
+    demo: "https://lanzzer-react.vercel.app/",
+    sourceCode: "https://github.com/Lanzzers/Lanzzer-React",
+    description:
+      "Lanzzer is a student first freelancing platform. one stop solution for students to become professional freelancers with hassle free invoices, quotations and contracts.",
+  },
+  {
+    id: 3,
+    name: "Agrim Wear",
+    slug: "agrimwear",
+    img: "agrimwear",
+    demo: "https://agrimwear.vercel.app/",
+    sourceCode: "https://github.com/Agrim-Sharma174/agrimwear",
+    description:
+      "An e-commerce website built using NextJs, TailwindCSS. It has features of cart, wishlist, checkout, payment,  etc. It is deployed on Vercel.",
+  },
+  {
+    id: 4,
     name: "Agrim-Travel",
     slug: "agrim-travel",
     img: "agrimtravel",
@@ -30,7 +40,7 @@ export const Projects = [
       "The website is made using NextJs and TailwindCSS with features of searching the hotels in a particular city and having server side rendering for the same. It is deployed on Vercel.",
   },
   {
-    id: 3,
+    id: 5,
     name: "Agrim Crypto Devs DAO",
     slug: "AgrimCryptoDevsDAO",
     img: "AgrimCryptoDevsDAO",
@@ -40,7 +50,7 @@ export const Projects = [
       "A full-stack dApp, frontend made with NextJs and backend with Solidity and Hardhat, using the quicknode.This project enables NFT holders to propose and vote on using their ETH to purchase NFTs from a marketplace, while also engaging in price speculation. Additionally, future profits from selling the NFTs are shared among all members of the DAO. ",
   },
   {
-    id: 4,
+    id: 6,
     name: "Initial Coin Offering",
     slug: "Initial-Coin-Offering-Dapp",
     img: "InitialCoinOfferingDapp",
@@ -50,7 +60,7 @@ export const Projects = [
       "A full-stack dApp made using NextJs, CSS and solidity, hardhat in backend. In this project, all existing NFT holders will receive the token for free, while allowing external individuals to purchase the token using ETH.",
   },
   {
-    id: 5,
+    id: 7,
     name: "NFT Collection dApp",
     slug: "NFT-Collection-Dapp",
     img: "NFTCollectionDapp",
@@ -60,7 +70,7 @@ export const Projects = [
       "This project aims to create a limited collection of 20 unique Crypto Dev NFTs. Users can mint only one NFT per transaction, ensuring exclusivity. Whitelisted users enjoy a 5-minute presale period before the public sale, guaranteeing them the opportunity to purchase one NFT per transaction. Additionally, a dedicated website will be created for the NFT collection.",
   },
   {
-    id: 6,
+    id: 8,
     name: "Whitelist Dapp",
     slug: "My-Whitelist-Dapp",
     img: "MyWhitelistDapp",
@@ -70,7 +80,7 @@ export const Projects = [
       "A full-stack dApp, with NextJs, solidity, harhat. This project focuses on providing early supporters with exclusive access to a whitelist for the Agrim Crypto Devs collection.",
   },
   {
-    id: 7,
+    id: 9,
     name: "Decentralized Exchange",
     slug: "agrim-decentralized-exchange",
     img: "agrim-dex",
@@ -80,7 +90,7 @@ export const Projects = [
       "The objective is to construct a decentralized exchange featuring a single asset pair (ETH/Crypto Dev). The exchange will charge a 1% fee on swaps. When users contribute liquidity, they will receive Crypto Dev LP tokens in return, proportionate to the amount of Ether they provide for liquidity.",
   },
   {
-    id: 6,
+    id: 10,
     name: "Full-stack dApps",
     slug: "dApp-projects",
     img: "smartcontracts",
@@ -90,7 +100,7 @@ export const Projects = [
       "A collection of some dApps made using solidity, hardhat, NextJs, TailwindCSS. These dApps are made for learning purpose. Some of them are- FIR complaint dApp, vending machine dApp, voting dApp, blog dApp, to-do dApp, etc.",
   },
   {
-    id: 8,
+    id: 11,
     name: "Coming Soon: The Cricket Game",
     slug: "coming-soon-project",
     img: "coming-soon",
