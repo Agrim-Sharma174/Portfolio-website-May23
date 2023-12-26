9b293b93fc0359db72b218f2fc7d06bc990db9bc
Agrim <agrimsharma1714@gmail.com>
2023-12-27 00:42:46 +0530

    loading page

--- components/LoadingPage.js
+++ components/LoadingPage.js
@@ -12,18 +12,7 @@ const LoadingPage = () => {
   return (
     <Layouts>
       <div className="relative flex h-screen w-full items-center justify-center">
-        <svg width="100" height="100" viewBox="0 0 207 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <AnimatePresence>
-            {show && (
-              <motion.path
-                {...SvgAnimation}
-                d="M0 841L0 -0.89001465L595.276  "
-                strokeWidth="10"
-                className="stroke-primary-light"
-              />
-            )}
-          </AnimatePresence>
-        </svg>
+        .
       </div>
     </Layouts>
   );
